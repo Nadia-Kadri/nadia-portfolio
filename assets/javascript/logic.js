f88591cde5ad97f16213dfd1435ff7243bb69610
Nadia-Kadri <nadiagrace96@gmail.com>
2020-07-02 20:39:31 -0400

    slick slider

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,34 +1,70 @@
 // Animate on Scroll Library initialization
-AOS.init({
-    // Global settings:
-    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
-    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
-    initClassName: 'aos-init', // class applied after initialization
-    animatedClassName: 'aos-animate', // class applied on animation
-    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
-    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
-    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
-    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+// AOS.init({
+//     // Global settings:
+//     disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+//     startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+//     initClassName: 'aos-init', // class applied after initialization
+//     animatedClassName: 'aos-animate', // class applied on animation
+//     useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+//     disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+//     debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+//     throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
 
 
-    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
-    offset: 120, // offset (in px) from the original trigger point
-    delay: 0, // values from 0 to 3000, with step 50ms
-    duration: 2000, // values from 0 to 3000, with step 50ms
-    easing: 'ease', // default easing for AOS animations
-    once: false, // whether animation should happen only once - while scrolling down
-    mirror: false, // whether elements should animate out while scrolling past them
-    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+//     // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+//     offset: 120, // offset (in px) from the original trigger point
+//     delay: 0, // values from 0 to 3000, with step 50ms
+//     duration: 2000, // values from 0 to 3000, with step 50ms
+//     easing: 'ease', // default easing for AOS animations
+//     once: false, // whether animation should happen only once - while scrolling down
+//     mirror: false, // whether elements should animate out while scrolling past them
+//     anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
 
+// });
+
+// $(document).on('click', 'a[href^="#"]', function (event) {
+//     event.preventDefault();
+
+//     $('html, body').animate({
+//         scrollTop: $($.attr(this, 'href')).offset().top
+//     }, 500);
+// });
+
+$(document).ready(function(){
+  $('.data-slick-center').slick({
+    autoplay: true,
+    autoplaySpeed: 2000,
+    speed: 2000,
+    dots: true,
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 2,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
 });
 
-$(document).on('click', 'a[href^="#"]', function (event) {
-    event.preventDefault();
 
-    $('html, body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 500);
-});
+
+
 
 
   
